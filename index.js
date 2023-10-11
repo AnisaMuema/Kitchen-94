0b5b150cd8d8ffc8d831c45e5ae8585ce4d1fbeb
anisamuema <anisa.muema.school@gmail.com>
2023-10-11 15:33:24 +0300

    Updated Laugh-it-out

---
+++ index.js
@@ -0,0 +1,16 @@
+const jokeContainer = document.getElementById("joke")
+const btn = document.getElementById("joke-btn")
+
+let getJoke = () => {
+    //this removes the fade effect after the joke is already displayed
+    jokeContainer.classList.remove("fade")
+    fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,political,racist,sexist,explicit&type=single")
+    .then((res) => res.json())
+    .then((data) => {
+        jokeContainer.textContent = `${data.joke}`
+        //this adds the fade as the joke is being displayed
+        jokeContainer.classList.add("fade")
+    })
+}
+btn.addEventListener("click", getJoke)
+getJoke();
