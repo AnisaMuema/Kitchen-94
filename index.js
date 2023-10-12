98709f1f860ff94e790a1d16cadb15cad39ea550
anisamuema <anisa.muema.school@gmail.com>
2023-10-12 13:56:08 +0300

    Completed project

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const jokeContainer = document.getElementById("joke")
 const btn = document.getElementById("joke-btn")
 
+
+
 let getJoke = () => {
+
+    function clearRadio(){
+        let resetRado = document.querySelectorAll('input[id="rate"]')
+        for(let i = 0; i < resetRado; i ++){
+            resetRado[i].checked = false;
+        }
+    }
+    clearRadio();
     //this removes the fade effect after the joke is already displayed
     jokeContainer.classList.remove("fade")
     fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,political,racist,sexist,explicit&type=single")
@@ -11,10 +21,19 @@ let getJoke = () => {
         //this adds the fade as the joke is being displayed
         jokeContainer.classList.add("fade")
     })
+    
+    
+    
 }
-btn.addEventListener("click", getJoke)
 
-// const widget = document.getElementById("star-widget") 
-
+btn.addEventListener("click", getJoke, clearRadio)
 getJoke();
 
+// function clearRadio(){
+//     let resetRado = document.querySelectorAll('input[id="rate"]')
+//     for(let i = 0; i < resetRado; i ++){
+//         resetRado[i].checked = false;
+//     }
+// }
+
+// btn.addEventListener("click", getJoke, clearRadio)
